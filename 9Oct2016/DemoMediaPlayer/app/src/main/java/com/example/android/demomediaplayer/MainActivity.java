2deb63ace8dbda31c25e7c8be9440b01b3b1f36f
zeeshanhanif <zeeshanhanif@gmail.com>
2016-10-16 16:40:37 +0500

    Class work of 9th October 2016

---
+++ 9Oct2016/DemoMediaPlayer/app/src/main/java/com/example/android/demomediaplayer/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.android.demomediaplayer;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    MediaPlayer mediaPlayer;
+    Button play,pause;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mediaPlayer = MediaPlayer.create(MainActivity.this,R.raw.song);
+
+        play = (Button) findViewById(R.id.play_button);
+        pause = (Button) findViewById(R.id.pause_button);
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.start();
+            }
+        });
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.pause();
+            }
+        });
+    }
+}
